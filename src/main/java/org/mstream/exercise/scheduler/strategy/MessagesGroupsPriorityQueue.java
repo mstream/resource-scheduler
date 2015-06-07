8fd81ba3f47b0782913046ac8bc0ba0045780a88
Laciak, Maciej <Maciej.Laciak@sabre.com>
2015-06-08 00:28:34 +0200

    Removes queue rebuilding in MessagesGroupPriorityQueue after comparator change.

--- src/main/java/org/mstream/exercise/scheduler/strategy/MessagesGroupsPriorityQueue.java
+++ src/main/java/org/mstream/exercise/scheduler/strategy/MessagesGroupsPriorityQueue.java
@@ -10,18 +10,19 @@ public class MessagesGroupsPriorityQueue implements QueuePrioritizationStrategy 
 
 	private List groupsOrder = new ArrayList( );
 	private Queue<Message> messageQueue;
-	private Comparator<Message> comparator = ( m1, m2 ) ->
-			Integer.compare( groupsOrder.indexOf( m1.getGroupId( ) ), groupsOrder.indexOf( m2.getGroupId( ) ) );
+	private Comparator<Message> comparator;
 
 	public MessagesGroupsPriorityQueue( ) {
-		rebuildQueue( );
+		comparator = ( m1, m2 ) -> Integer.compare(
+				groupsOrder.indexOf( m1.getGroupId( ) ),
+				groupsOrder.indexOf( m2.getGroupId( ) ) );
+		messageQueue = new PriorityQueue<>( comparator );
 	}
 
 	@Override public void enqueue( Message message ) {
 		Object groupId = message.getGroupId( );
 		if ( !groupsOrder.contains( groupId ) ) {
 			groupsOrder.add( groupId );
-			rebuildQueue( );
 		}
 		messageQueue.offer( message );
 	}
@@ -46,11 +47,4 @@ public class MessagesGroupsPriorityQueue implements QueuePrioritizationStrategy 
 		messageQueue = newMessageQueue;
 	}
 
-	private void rebuildQueue( ) {
-		Queue<Message> newMessageQueue = new PriorityQueue<>( comparator );
-		if ( messageQueue != null ) {
-			newMessageQueue.addAll( messageQueue );
-		}
-		messageQueue = newMessageQueue;
-	}
 }
